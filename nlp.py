90e837ab1cb90b7129d57e98f10fcb3214b05ab5
Pete Richardson <pete@peterichardson.com>
2018-10-23 15:15:28 -0500

    extracted end_year and end_reason

--- nlp.py
+++ nlp.py
@@ -14,17 +14,21 @@ with open("ships.csv", "rb") as f:
 		ship = {}
 		ship['events']=[]
 		ship['notes'] = []
-		(ship['year'], ship['name'], ship['guns'], ship['rating'],ship['misc']) = line.split(",")
+		(ship['start_year'], ship['name'], ship['guns'], ship['rating'],ship['misc']) = line.split(",")
 		for c in extract(ship['misc']):
-			ship['year'] = ""
+			ship['start_year'] = ""
 			ship['text'] = c.strip()
 			#m = re.match("(.*) ([\d-]+)( \[[0-9\[\]]\])?$",c)
 			m = re.match("(.*) ?([\d-]{4})(-\d+)? ?\[?.*\]?$",c)
 			if m:
-				ship['text'],ship['year'],_ = m.groups()
-				event = (ship['year'], ship['text'].strip())
-				#ship['events'].append("{}: {}".format(ship['year'], ship['text'].strip()))
+				ship['text'],ship['start_year'],_ = m.groups()
+				ship['text'] = ship['text'].strip()
+				event = (ship['start_year'], ship['text'])
 				ship['events'].append(event)
+				if ship['text'] in ['sunk by the Luftwaffe','burnt and broken up','cancelled','destroyed by fire','broken up','sold','scuttled','foundered','hulked',
+				'sold for breaking', 'wreck sold for breaking']:
+					ship['end_year'] = ship['start_year']
+					ship['end_reason'] = ship['text']
 			else:
 				if c[0:2]=="ex-":
 					ex = c[3:]
@@ -32,4 +36,9 @@ with open("ships.csv", "rb") as f:
 					ship['notes'].append(ship['text'].strip())
 					#print("\twas {}".format(c.strip()))
 					#print("\t----{}".format(ship['text'].strip()))
+		ship.pop('notes','')
+		ship.pop('misc','')
+		ship.pop('text','')
+		#if not ship.get('end_year',None):
+		#	print('-'*50)
 		pprint(ship)
