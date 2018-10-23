f63b771329c4f0d86e2048f0f73eef6f4e0c339c
Pete Richardson <pete@peterichardson.com>
2018-10-23 12:29:47 -0500

    better parsing of notes

--- nlp.py
+++ nlp.py
@@ -1,22 +1,35 @@
 #!/usr/bin/env python
 
 import re
+from pprint import pprint
 
 def extract(notes):
 	notes = notes.replace(";",".")
 	clauses = notes.split(".")
 	return clauses
 
+
 with open("ships.csv", "rb") as f:
 	for line in f.readlines():
-		(year, name, guns, rating, notes) = line.split(",")
-		print("{}\n{}".format("-"*40, name))
-		for c in extract(notes):
-			c_year = ""
-			c_text = c
-			m = re.match("(.*) ([\d-]+)( \[[0-9\[\]]\])?$",c)
+		ship = {}
+		ship['events']=[]
+		ship['notes'] = []
+		(ship['year'], ship['name'], ship['guns'], ship['rating'],ship['misc']) = line.split(",")
+		for c in extract(ship['misc']):
+			ship['year'] = ""
+			ship['text'] = c.strip()
+			#m = re.match("(.*) ([\d-]+)( \[[0-9\[\]]\])?$",c)
+			m = re.match("(.*) ?([\d-]{4})(-\d+)? ?\[?.*\]?$",c)
 			if m:
-				c_text,c_year,c_citations = m.groups()
-				print("\t{}: {}".format(c_year, c_text.strip()))
+				ship['text'],ship['year'],_ = m.groups()
+				event = (ship['year'], ship['text'].strip())
+				#ship['events'].append("{}: {}".format(ship['year'], ship['text'].strip()))
+				ship['events'].append(event)
 			else:
-				print("\t{}".format(c_text.strip()))
+				if c[0:2]=="ex-":
+					ex = c[3:]
+				else:				
+					ship['notes'].append(ship['text'].strip())
+					#print("\twas {}".format(c.strip()))
+					#print("\t----{}".format(ship['text'].strip()))
+		pprint(ship)
