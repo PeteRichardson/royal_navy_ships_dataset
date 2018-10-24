ddbd77a214c38ebd320fed30c15b902171792665
Pete Richardson <pete@peterichardson.com>
2018-10-24 13:14:07 -0500

    refactored into Ship class

--- nlp.py
+++ nlp.py
@@ -13,9 +13,59 @@ APP_NAME = "NLP"
 logger = logging.getLogger("NLP")
 logging.basicConfig(level=logging.INFO)
 
+class Ship(object):
+	def __init__(self, ship_id, text):
+		self.id = ship_id
+		self.events=[]
+		self.notes = []
+		self.start_year = 9999
+		self.end_year = 9999
+		self.end_reason = ""
+		logger.debug(text)
+		(self.start_year, self.name, self.guns, self.rating,self.misc) = text.split(",")
+		for c in extract_clauses(self.misc):
+			self.text = c.strip()
+			if (self.start_year != '-') and (self.start_year != '?'):
+				self.start_year = int(self.start_year)
+			#m = re.match("(.*) ([\d-]+)( \[[0-9\[\]]\])?$",c)
+			m = re.match("(.*) ?([\d]{4})(-\d+)? ?\[?.*\]?$",c)
+			if m:
+				self.text,self.start_year,_ = m.groups()
+				self.text = self.text.strip()
+				self.start_year = int(self.start_year)
+				event = (self.start_year, self.text)
+				self.events.append(event)
+				if self.text in ['sunk by the Luftwaffe',
+									'burnt and broken up',
+									'cancelled',
+									'destroyed by fire',
+									'broken up',
+									'sold',
+									'scuttled',
+									'foundered',
+									'hulked',
+									'sold for breaking',
+									'wreck sold for breaking']:
+					self.end_year = int(self.start_year)
+					self.end_reason = self.text
+			else:
+				if c[0:2]=="ex-":
+					ex = c[3:]
+				else:				
+					self.notes.append(self.text.strip())
+					#print("\twas {}".format(c.strip()))
+					#print("\t----{}".format(self.text'].strip()))
+		#ship.pop('notes','')
+		self.notes = ". ".join(self.notes)
+		self.misc = None
+		self.text = None
+		#if not ship.get('end_year',None):
+		#	print('-'*50)
+		#pprint(ship)
+
 extract = None
 
-def define_schema():
+def define_schema(extract):
 	# Define Table Schema (If we are creating a new extract)
 	# (NOTE: In Tableau Data Engine, all tables must be named 'Extract')
 	logger.debug("Defining Schema")
@@ -38,82 +88,36 @@ def extract_clauses(notes):
 	clauses = notes.split(".")
 	return clauses
 
-
 def create_row(schema, ship):
 	row = Row(schema)
 	t = datetime.now().timetuple()
-	row.setInteger(0, ship['id'])
-	row.setCharString(1, ship['name'])
-	row.setCharString(2, ship['rating'])
-	row.setInteger(3, int(ship['guns']))
-	row.setDateTime(4, int(ship.get('start_year',9999)), 1, 1, 12, 0, 0, 0)
-	row.setDateTime(5, int(ship.get('end_year',9999)), 1, 1, 12, 0, 0, 0)
-	row.setCharString(6, ship.get('end_reason',""))
-	row.setCharString(7, ship.get('notes',""))
+	row.setInteger(0, ship.id)
+	row.setCharString(1, ship.name)
+	row.setCharString(2, ship.rating)
+	row.setInteger(3, int(ship.guns))
+	row.setDateTime(4, int(ship.start_year), 1, 1, 12, 0, 0, 0)
+	row.setDateTime(5, int(ship.end_year), 1, 1, 12, 0, 0, 0)
+	row.setCharString(6, ship.end_reason)
+	row.setCharString(7, ship.notes)
 
-	logger.info("Inserting ship {}. {}".format(ship['id'], ship['name']))
+	logger.info("Inserting ship {}. {}".format(ship.id, ship.name))
 	return row
 
 ExtractAPI.initialize()
 
 output_filename = "ships.hyper"
-
 os.unlink(output_filename)
 
 extract = Extract(output_filename)
 
-define_schema()
+define_schema(extract)
 table = extract.openTable('Extract')
 schema = table.getTableDefinition()
 
 with open("ships.csv", "rb") as f:
 	ship_id = 1
 	for line in f.readlines()[1:]:     # [1:] skips the header line in the csv
-		ship = {}
-		ship['id'] = ship_id
-		ship['events']=[]
-		ship['notes'] = []
-		logger.debug(line)
-		(ship['start_year'], ship['name'], ship['guns'], ship['rating'],ship['misc']) = line.split(",")
-		for c in extract_clauses(ship['misc']):
-			ship['text'] = c.strip()
-			if (ship['start_year'] != '-') and (ship['start_year'] != '?'):
-				ship['start_year'] = int(ship['start_year'])
-			#m = re.match("(.*) ([\d-]+)( \[[0-9\[\]]\])?$",c)
-			m = re.match("(.*) ?([\d]{4})(-\d+)? ?\[?.*\]?$",c)
-			if m:
-				ship['text'],ship['start_year'],_ = m.groups()
-				ship['text'] = ship['text'].strip()
-				ship['start_year'] = int(ship['start_year'])
-				event = (ship['start_year'], ship['text'])
-				ship['events'].append(event)
-				if ship['text'] in ['sunk by the Luftwaffe',
-									'burnt and broken up',
-									'cancelled',
-									'destroyed by fire',
-									'broken up',
-									'sold',
-									'scuttled',
-									'foundered',
-									'hulked',
-									'sold for breaking',
-									'wreck sold for breaking']:
-					ship['end_year'] = int(ship['start_year'])
-					ship['end_reason'] = ship['text']
-			else:
-				if c[0:2]=="ex-":
-					ex = c[3:]
-				else:				
-					ship['notes'].append(ship['text'].strip())
-					#print("\twas {}".format(c.strip()))
-					#print("\t----{}".format(ship['text'].strip()))
-		#ship.pop('notes','')
-		ship['notes'] = ". ".join(ship['notes'])
-		ship.pop('misc','')
-		ship.pop('text','')
-		#if not ship.get('end_year',None):
-		#	print('-'*50)
-		#pprint(ship)
+		ship = Ship(ship_id, line)
 		row = create_row(schema, ship)
 		ship_id = ship_id + 1
 		table.insert(row)
