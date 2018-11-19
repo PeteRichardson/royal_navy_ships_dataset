47d8011ce9e40dd0108345f199c82f03ccb9b515
Pete Richardson <pete@peterichardson.com>
2018-11-18 17:23:56 -0800

    successfully scraping from dbpedia

--- getships.py
+++ getships.py
@@ -3,6 +3,11 @@ import aiohttp
 import signal
 import sys
 import json
+from pprint import pprint
+from csv import DictReader
+import logging
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 
 
@@ -18,21 +23,43 @@ async def get_json(url):
         else:
             return ""
 
+
+def extract_details(detail_json):
+    details = {}
+    dbos = ['class', 'homeport', 'layingDown', 'length', 'orderDate', 'shipBeam',
+              'shipLaunch', 'status', 'topSpeed', 'shipCommissioned', 'shipComplement',
+               'shipTonsBurthen']
+
+    dbps = ['shipArmament','shipDisplacement', 'shipPropulsion', 'shipArmour', 'shipNamesake', 'shipAcquired',
+                  'shipHonours', 'shipBuilder', 'shipCountry', 'shipCompleted', 'shipLaidDown', 'shipComplement']
+
+    for f in dbos:
+        if 'http://dbpedia.org/ontology/{}'.format(f) in detail_json:
+            details[f] = detail_json['http://dbpedia.org/ontology/{}'.format(f)][0]['value']
+    for f in dbps:
+        if 'http://dbpedia.org/property/{}'.format(f) in detail_json:
+            details[f] = detail_json['http://dbpedia.org/property/{}'.format(f)][0]['value']
+    return details
+
 async def get_dbpedia(shipname):
     data = await get_json(search_url.format(shipname))
     j = json.loads(data)['results']
     for s in j:
-        print("--- ship: {}    {}".format(s['label'],s['uri']))
         detail_data = await get_json(s['uri'])
         if len(detail_data) > 10:
+            print("--- ship: {}    {}".format(s['label'],s['uri']))
             detail_json = json.loads(detail_data)[s['uri']]
-            if 'http://dbpedia.org/ontology/topSpeed' in detail_json:
-                print("found topSpeed")
-                print(detail_json['http://dbpedia.org/ontology/topSpeed'][0]['value'])
+            details = extract_details(detail_json)
+            pprint(details)
 
 async def get_all_ships():
-    shipnames = ['Victory', 'Prince', 'Royal James']
-    for shipname in shipnames:
+    fieldnames = ['year_launched','name','guns','rating','notes']
+    ships = {}
+    with open("ships.csv","r") as f:
+        ships = { row['name']:row for row in DictReader(f) }
+
+    for shipname in sorted(ships.keys()):
+        logger.debug("dbpedia: {}".format(shipname))
         await asyncio.ensure_future(get_dbpedia(shipname))
     await client.close()
 
