1855da2d66e0a0463ce19cf3f7d02c6cda4caaa7
Pete Richardson <pete@peterichardson.com>
2026-07-24 12:05:07 -0700

    Remove deprecated tableausdk Hyper export from nlp.py

--- nlp.py
+++ nlp.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python
 
 import re
-import os
 from pprint import pprint
-from tableausdk import Collation, Type
-from tableausdk.HyperExtract import ExtractAPI, Extract, TableDefinition, Row
-from datetime import datetime
 import logging
 
 
@@ -72,66 +68,17 @@ class Ship(object):
 	def __str__(self):
 		return "[{:4}] {} ({}, {}g)".format(self.id, self.name, self.start_year, self.guns)
 
-def define_schema(extract):
-	# Define Table Schema (If we are creating a new extract)
-	# (NOTE: In Tableau Data Engine, all tables must be named 'Extract')
-	logger.debug("Defining Schema")
-
-	if not extract.hasTable('Extract'):
-		schema = TableDefinition()
-		schema.setDefaultCollation(Collation.EN_US)
-		schema.addColumn('ID', Type.INTEGER)
-		schema.addColumn('Name', Type.CHAR_STRING)
-		schema.addColumn('Rating', Type.CHAR_STRING)
-		schema.addColumn('Guns', Type.INTEGER)
-		schema.addColumn('Start Year', Type.DATETIME)
-		schema.addColumn('End Year', Type.DATETIME)
-		schema.addColumn('End Reason', Type.CHAR_STRING)
-		schema.addColumn('Notes', Type.CHAR_STRING)
-		extract.addTable('Extract', schema)
-
-def create_row(schema, ship):
-	row = Row(schema)
-	t = datetime.now().timetuple()
-	row.setInteger(0, ship.id)
-	row.setCharString(1, ship.name)
-	row.setCharString(2, ship.rating)
-	row.setInteger(3, int(ship.guns))
-	row.setDateTime(4, int(ship.start_year), 1, 1, 12, 0, 0, 0)
-	row.setDateTime(5, int(ship.end_year), 1, 1, 12, 0, 0, 0)
-	row.setCharString(6, ship.end_reason)
-	row.setCharString(7, ship.notes)
-	return row
-
 if __name__ == '__main__':
 	logger = logging.getLogger("NLP")
 	logging.basicConfig(level=logging.INFO)
 
 	with open("ships.csv", "rb") as f:
-		ExtractAPI.initialize()
-
-		output_filename = "ships.hyper"
-		os.unlink(output_filename)
-
-		extract = Extract(output_filename)
-
-		define_schema(extract)
-		table = extract.openTable('Extract')
-		schema = table.getTableDefinition()
-
 		ship_id = 0
 		for line in f.readlines()[1:]:     # [1:] skips the header line in the csv
 			ship_id = ship_id + 1
 			ship = Ship(ship_id, line)
-			row = create_row(schema, ship)
-			table.insert(row)
 
 			logger.info(ship)
 			logger.debug(ship.notes)
 			for e in ship.events:
 				logger.debug(e)
-
-
-		extract.close()
-
-		ExtractAPI.cleanup()
